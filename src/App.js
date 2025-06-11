a03677918e9171ae2cd8f03976972671e5499acc
YeetusPapitus <markosego5@gmail.com>
2025-06-11 16:59:22 +0200

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-// src/App.js
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Video from './pages/Video';
-import Category from './pages/Category';
-import AdminLogin from './pages/AdminLogin';
-import AdminPage from './pages/AdminPage';
-import PrivateRoute from './components/PrivateRoute';
-
+const basename = '';  // replace with your GitHub repo name
 
 function App() {
   return (
-    <Router>
+    <Router basename={basename}>
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <Routes>
@@ -40,6 +28,3 @@ function App() {
     </Router>
   );
 }
-
-
-export default App;
